31f1979026dabc2acd5d465bc91e5f1b0cf93f80
sunpinenuts <904975251@qq.com>
2023-05-14 10:00:33 +0800

    项目构建

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { getCategoryAPI } from '@/api/test.js'
+import { onMounted } from 'vue'
+const getTestData = async () => {
+  const result = await getCategoryAPI()
+  console.log(result)
+}
+onMounted(() => {
+  getTestData()
+})
+</script>
+
+<template>
+  <div class="box"></div>
+  <RouterView />
+</template>
+<style lang="scss" scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background: $xtxColor;
+}
+</style>
